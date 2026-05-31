3b41ed61405930eea4ad366ecf70605dc73f2bff
dab221 <dabdamian22@gmail.com>
2026-05-31 17:03:31 -0300

    Agregando comentario (Que hace la linea de codigo)

--- app/src/main/java/com/appuntes/domain/model/Tarea.kt
+++ app/src/main/java/com/appuntes/domain/model/Tarea.kt
@@ -6,16 +6,16 @@ enum class PrioridadTarea { BAJA, MEDIA, ALTA }
 enum class EstadoTarea { PENDIENTE, EN_PROGRESO, COMPLETADA }
 
 data class Tarea(
-    val id: Long = 0, // ID Único
+    val id: Long = 0, // ID Único (Room la utoimcrementa)
     val titulo: String, // Título obligatorio
-    val descripcion: String = "",
-    val materiaId: Long,
+    val descripcion: String = "", //Descripción opcional
+    val materiaId: Long, //Referencia a la materia
     val materiaNombre: String = "",
     val materiaColor: String = "#6750A4",
-    val fechaEntrega: LocalDate,
+    val fechaEntrega: LocalDate,   // Fecha de Vencimiento
     val prioridad: PrioridadTarea = PrioridadTarea.MEDIA,
     val estado: EstadoTarea = EstadoTarea.PENDIENTE,
-    val imagenUri: String? = null,
+    val imagenUri: String? = null, // Foto de camara (Puede ser Null)
     val notaVoz: String? = null,
     val recordatorioActivo: Boolean = false,
     val fechaCreacion: LocalDate = LocalDate.now()
